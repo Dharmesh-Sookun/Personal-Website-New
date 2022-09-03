9c19f0313637ec259697990c83b136b17e1fdeda
Dharmesh-Sookun <sookundharmesh2906@gmail.com>
2022-09-03 18:39:35 +0400

    Added active links in navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { Link, useLocation } from 'react-router-dom';
 import './Navigation.css';
 
 function Navigation() {
+  const location = useLocation();
   return (
     <div className="navigation">
       <h3>
@@ -13,22 +13,42 @@ function Navigation() {
       </h3>
       <ul>
         <li>
-          <Link className="nav-links" to="/">
+          <Link
+            className={
+              location.pathname == '/' ? 'active-nav-links' : 'nav-links'
+            }
+            to="/"
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link className="nav-links" to="/about">
+          <Link
+            className={
+              location.pathname == '/about' ? 'active-nav-links' : 'nav-links'
+            }
+            to="/about"
+          >
             About
           </Link>
         </li>
         <li>
-          <Link className="nav-links" to="/resume">
+          <Link
+            className={
+              location.pathname == '/resume' ? 'active-nav-links' : 'nav-links'
+            }
+            to="/resume"
+          >
             Resume
           </Link>
         </li>
         <li>
-          <Link className="nav-links" to="/contact">
+          <Link
+            className={
+              location.pathname == '/contact' ? 'active-nav-links' : 'nav-links'
+            }
+            to="/contact"
+          >
             Contact
           </Link>
         </li>
